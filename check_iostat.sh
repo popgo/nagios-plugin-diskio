327430d53b4f1e58570c345403fa13dca6ad8309
popgo <unidba@gmail.com>
2013-11-08 13:43:30 +0800

    1.4.8 - Added I/O util% feature by iostat -x

--- check_iostat.sh
+++ check_iostat.sh
@@ -20,6 +20,7 @@
 # Updated: 2008-11-27 (i.yates@uea.ac.uk) - Release 1.4.5 - Thanks to Alexander Kaufmann for updates!
 # Updated: 2010-11-04 (os@ciphron.de) - 1.4.6 - Fixed little bug (CAT <-> cat), added check if device exist (return: UNKNOWN)
 # Updated: 2010-11-12 (os@ciphron.de) - 1.4.7 - Added check if iostat is installed, added sudo for iostat
+# Updated: 2013-01-28 (unidba@gmail.com) - 1.4.8 - Added I/O util% feature by iostat -x
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -36,16 +37,23 @@
 # 
 ###############################################
 
-. ./utils.sh
+#. ./utils.sh
 
-VERSION="1.4.7"
+VERSION="1.4.8"
 
 IOSTAT=`which iostat 2>/dev/null`
 GREP=`which grep 2>/dev/null`
 AWK=`which awk 2>/dev/null`
 TAIL=`which tail 2>/dev/null`
 
+STATE_OK=0
+STATE_WARNING=1
+STATE_CRITICAL=2
+STATE_UNKNOWN=3
+STATE_DEPENDENT=4
+
 FLAG_VERBOSE=FALSE
+FLAG_UTIL=FALSE
 FLAG_TPS=FALSE
 FLAG_READS=FALSE
 FLAG_WRITES=FALSE
@@ -66,12 +74,13 @@ EXIT_STATUS=$STATE_OK
 usage() {
 	echo " check_iostat $VERSION - Nagios I/O statistics check script"
 	echo ""
-	echo " Usage: check_iostat -w <warning value> -c <critical value> -l <Number of samples> -t|i|o|a <device> [ -v ] [ -h ]"
+	echo " Usage: check_iostat -w <warning value> -c <critical value> -l <Number of samples> -u|t|i|o|a <device> [ -v ] [ -h ]"
 	echo ""
 	echo " NOTE: When specifying device, /dev/ is assumed, e.g. for /dev/sda you should just enter sda for the device"
 	echo ""
 	echo "		-w  Warning trigger level"
 	echo "		-c  Critical trigger level"
+	echo "		-u  I/O util% on <device>"
 	echo "		-t  I/O transactions per second (TPS/IOPS) on <device>"
 	echo "		-i  Kilobytes read IN per second on <device>"
 	echo "		-o  Kilobytes written OUT per second on <device>"
@@ -86,7 +95,7 @@ usage() {
 doopts() {
 	if ( `test 0 -lt $#` )
 	then
-		while getopts w:c:l:t:i:o:a:vh myarg "$@"
+		while getopts w:c:l:u:t:i:o:a:vh myarg "$@"
 		do
 			case $myarg in
 				h|\?)
@@ -96,6 +105,9 @@ doopts() {
 					LEVEL_WARN=$OPTARG;;
 				c)
 					LEVEL_CRIT=$OPTARG;;
+                                u)
+                                        FLAG_UTIL=TRUE
+                                        DEVICE=$OPTARG;;
 				t)
 					FLAG_TPS=TRUE
 					DEVICE=$OPTARG;;
@@ -165,7 +177,12 @@ fi
 # OS at ciphron.de: 2010-11-04 
 # - Added sudo
 if [ $COUNT -ge 2 ] ; then
-	sudo $IOSTAT -k -d $DEVICE 1 $COUNT | $GREP $DEVICE| $TAIL -`expr $COUNT - 1` > $TMP
+	if [ $FLAG_UTIL = "TRUE" ] ; then
+		$IOSTAT -x -d $DEVICE 1 $COUNT | $GREP $DEVICE| $TAIL -`expr $COUNT - 1` > $TMP
+	else
+		#sudo $IOSTAT -k -d $DEVICE 1 $COUNT | $GREP $DEVICE| $TAIL -`expr $COUNT - 1` > $TMP
+		$IOSTAT -k -d $DEVICE 1 $COUNT | $GREP $DEVICE| $TAIL -`expr $COUNT - 1` > $TMP
+	fi
 else
 	RESULT="ERROR Count must be > 1!"
 	EXIT_STATUS=$STATE_UNKNOWN
@@ -174,21 +191,46 @@ fi
 
 ## General sanity check
 if [ -n "$LEVEL_WARN" -a -n "$LEVEL_CRIT" -a "$LEVEL_WARN" -lt "$LEVEL_CRIT" ]; then
+	## I/O Util% per second
+	if [ $FLAG_UTIL = "TRUE" ] ; then
+		UTIL=`cat $TMP | $AWK '{ sum += $12 } END { print sum / NR } '`
+		UTIL_ABS=`echo $UTIL | $AWK '{printf "%d",$1}'`
+		RESULTPERF="on $DEVICE util%:$UTIL/% | util%=$UTIL;$LEVEL_WARN;$LEVEL_CRIT;0;;"
+		if [ "$UTIL_ABS" -lt "$LEVEL_WARN" ] ; then
+			RESULT="OK - $RESULTPERF"
+			EXIT_STATUS=$STATE_OK
+		else
+			if [ "$UTIL_ABS" -ge "$LEVEL_CRIT" ] ; then
+				RESULT="CRITICAL - $RESULTPERF"
+				EXIT_STATUS=$STATE_CRITICAL
+			else
+				if [ "$UTIL_ABS" -ge "$LEVEL_WARN" ] ; then
+					RESULT="WARNING - $RESULTPERF"
+					EXIT_STATUS=$STATE_WARNING
+				fi
+			fi
+		fi
+	fi
+
 	## Handle Transactions per second
 	if [ $FLAG_TPS = "TRUE" ] ; then
 		TPS=`cat $TMP | $AWK '{ sum += $2 } END { print sum / NR } '`
 		TPS_ABS=`echo $TPS | $AWK '{printf "%d",$1}'`
-		RESULTPERF="on $DEVICE| io=$TPS;$LEVEL_WARN;$LEVEL_CRIT;0;"
+		#RESULTPERF="on $DEVICE| io=$TPS;$LEVEL_WARN;$LEVEL_CRIT;0;"
+		RESULTPERF="on $DEVICE tps:$TPS/s | tps=$TPS;$LEVEL_WARN;$LEVEL_CRIT;0;;"
 		if [ "$TPS_ABS" -lt "$LEVEL_WARN" ] ; then
-			RESULT="IOSTAT OK - $RESULTPERF"
+			#RESULT="IOSTAT OK - $RESULTPERF"
+			RESULT="OK - $RESULTPERF"
 			EXIT_STATUS=$STATE_OK
 		else
 			if [ "$TPS_ABS" -ge "$LEVEL_CRIT" ] ; then 
-				RESULT="IOSTAT CRITICAL - $RESULTPERF"
+				#RESULT="IOSTAT CRITICAL - $RESULTPERF"
+				RESULT="CRITICAL - $RESULTPERF"
 				EXIT_STATUS=$STATE_CRITICAL
 			else
 				if [ "$TPS_ABS" -ge "$LEVEL_WARN" ] ; then 
-					RESULT="IOSTAT WARNING - $RESULTPERF"
+					#RESULT="IOSTAT WARNING - $RESULTPERF"
+					RESULT="WARNING - $RESULTPERF"
 					EXIT_STATUS=$STATE_WARNING
 				fi
 			fi
@@ -199,17 +241,21 @@ if [ -n "$LEVEL_WARN" -a -n "$LEVEL_CRIT" -a "$LEVEL_WARN" -lt "$LEVEL_CRIT" ]; 
 	if [ $FLAG_READS = "TRUE" ] ; then
 		READSS=`cat $TMP | $AWK '{ sum += $3} END { print sum / NR } '`
 		READSS_ABS=`echo $READSS | $AWK '{printf "%d",$1}'`
-		RESULTPERF="on $DEVICE| read=$READSS;$LEVEL_WARN;$LEVEL_CRIT;0;"
+		#RESULTPERF="on $DEVICE| read=$READSS;$LEVEL_WARN;$LEVEL_CRIT;0;"
+		RESULTPERF="on $DEVICE read:$READSS KB/s | read=$READSS;$LEVEL_WARN;$LEVEL_CRIT;0;;"
 		if [ "$READSS_ABS" -lt "$LEVEL_WARN" ] ; then
-			RESULT="IOSTAT OK $RESULTPERF"
+			#RESULT="IOSTAT OK $RESULTPERF"
+			RESULT="OK $RESULTPERF"
 			EXIT_STATUS=$STATE_OK
 		else
 			if [ "$READSS_ABS" -ge "$LEVEL_CRIT" ] ; then 
-				RESULT="IOSTAT CRITICAL $RESULTPERF"
+				#RESULT="IOSTAT CRITICAL $RESULTPERF"
+				RESULT="CRITICAL $RESULTPERF"
 				EXIT_STATUS=$STATE_CRITICAL
 			else
 				if [ "$READSS_ABS" -ge "$LEVEL_WARN" ] ; then 
-					RESULT="IOSTAT WARNING $RESULTPERF"
+					#RESULT="IOSTAT WARNING $RESULTPERF"
+					RESULT="WARNING $RESULTPERF"
 					EXIT_STATUS=$STATE_WARNING
 				fi
 			fi
@@ -220,17 +266,21 @@ if [ -n "$LEVEL_WARN" -a -n "$LEVEL_CRIT" -a "$LEVEL_WARN" -lt "$LEVEL_CRIT" ]; 
 	if [ $FLAG_WRITES = "TRUE" ] ; then
 		WRITESS=`cat $TMP | $AWK '{ sum += $4} END { print sum / NR } '`
 		WRITESS_ABS=`echo $WRITESS | $AWK '{printf "%d",$1}'`
-		RESULTPERF="on $DEVICE| write=$WRITESS;$LEVEL_WARN;$LEVEL_CRIT;0;"
+		#RESULTPERF="on $DEVICE| write=$WRITESS;$LEVEL_WARN;$LEVEL_CRIT;0;"
+		RESULTPERF="on $DEVICE write:$WRITESS KB/s | write=$WRITESS;$LEVEL_WARN;$LEVEL_CRIT;0;;"
 		if [ "$WRITESS_ABS" -lt "$LEVEL_WARN" ] ; then
-			RESULT="IOSTAT OK $RESULTPERF"
+			#RESULT="IOSTAT OK $RESULTPERF"
+			RESULT="OK $RESULTPERF"
 			EXIT_STATUS=$STATE_OK
 		else
 			if [ "$WRITESS_ABS" -ge "$LEVEL_CRIT" ] ; then 
-				RESULT="IOSTAT CRITICAL $RESULTPERF "
+				#RESULT="IOSTAT CRITICAL $RESULTPERF "
+				RESULT="CRITICAL $RESULTPERF "
 				EXIT_STATUS=$STATE_CRITICAL
 			else
 				if [ "$WRITESS_ABS" -ge "$LEVEL_WARN" ] ; then 
-					RESULT="IOSTAT WARNING $RESULTPERF"
+					#RESULT="IOSTAT WARNING $RESULTPERF"
+					RESULT="WARNING $RESULTPERF"
 					EXIT_STATUS=$STATE_WARNING
 				fi
 			fi
@@ -243,17 +293,21 @@ if [ -n "$LEVEL_WARN" -a -n "$LEVEL_CRIT" -a "$LEVEL_WARN" -lt "$LEVEL_CRIT" ]; 
 		READSS_ABS=`echo $READSS | $AWK '{printf "%d",$1}'`
 		WRITESS=`cat $TMP | $AWK '{ sum += $4 } END { print sum / NR } '`
 		WRITESS_ABS=`echo $WRITESS | $AWK '{printf "%d",$1}'`
-		RESULTPERF="on $DEVICE| read=$READSS;$LEVEL_WARN;$LEVEL_CRIT;0; write=$WRITESS;$LEVEL_WARN;$LEVEL_CRIT;0 "
+		#RESULTPERF="on $DEVICE| read=$READSS;$LEVEL_WARN;$LEVEL_CRIT;0; write=$WRITESS;$LEVEL_WARN;$LEVEL_CRIT;0 "
+		RESULTPERF="on $DEVICE read:$READSS KB/s write:$WRITESS KB/s| read=$READSS;$LEVEL_WARN;$LEVEL_CRIT;0;; write=$WRITESS;$LEVEL_WARN;$LEVEL_CRIT;0;;"
 		if [ "$READSS_ABS" -lt "$LEVEL_WARN" ] && [ "$WRITESS_ABS" -lt "$LEVEL_WARN" ]  ; then
-			RESULT="IOSTAT OK $RESULTPERF"
+			#RESULT="IOSTAT OK $RESULTPERF"
+			RESULT="OK $RESULTPERF"
 			EXIT_STATUS=$STATE_OK
 		else
 			if [ "$READSS_ABS" -ge "$LEVEL_CRIT" ] || [ "$WRITESS_ABS" -ge "$LEVEL_CRIT" ] ; then
-				RESULT="IOSTAT CRITICAL $RESULTPERF"
+				#RESULT="IOSTAT CRITICAL $RESULTPERF"
+				RESULT="CRITICAL $RESULTPERF"
 				EXIT_STATUS=$STATE_CRITICAL
 			else
 				if [ "$READSS_ABS" -ge "$LEVEL_WARN" ] || [ "$WRITESS_ABS" -ge "$LEVEL_WARN" ] ; then
-					RESULT="IOSTAT WARNING $RESULTPERF"
+					#RESULT="IOSTAT WARNING $RESULTPERF"
+					RESULT="WARNING $RESULTPERF"
 					EXIT_STATUS=$STATE_WARNING
 				fi
 			fi
